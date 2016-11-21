3f29aabcd432a9a7e24381b77fa0440681870e62
Robert Vilvert <robert.lvilvert@gmail.com>
2016-11-21 22:25:48 +0000

    Here we go!

---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://opencart.nextcloudservices.com/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://opencart.nextcloudservices.com/');
+
+// DIR
+define('DIR_APPLICATION', '/var/www/html/catalog/');
+define('DIR_SYSTEM', '/var/www/html/system/');
+define('DIR_LANGUAGE', '/var/www/html/catalog/language/');
+define('DIR_TEMPLATE', '/var/www/html/catalog/view/theme/');
+define('DIR_CONFIG', '/var/www/html/system/config/');
+define('DIR_IMAGE', '/var/www/html/image/');
+define('DIR_CACHE', '/var/www/html/system/storage/cache/');
+define('DIR_DOWNLOAD', '/var/www/html/system/storage/download/');
+define('DIR_LOGS', '/var/www/html/system/storage/logs/');
+define('DIR_MODIFICATION', '/var/www/html/system/storage/modification/');
+define('DIR_UPLOAD', '/var/www/html/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'docker.cmmcfluxl1um.sa-east-1.rds.amazonaws.com');
+define('DB_USERNAME', 'docker');
+define('DB_PASSWORD', 'opencartpass');
+define('DB_DATABASE', 'OPENCART');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
