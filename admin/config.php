369739bcbd33d1f03cf42ebc4cade0c3fd2feb59
robertvilvert <robert.lvilvert@gmail.com>
2016-11-22 09:00:58 -0200

    Update config.php

--- admin/config.php
+++ admin/config.php
@@ -22,11 +22,5 @@ define('DIR_UPLOAD', '/var/www/html/system/storage/upload/');
 define('DIR_CATALOG', '/var/www/html/catalog/');
 
 // DB
-define('DB_DRIVER', 'mysqli');
-define('DB_HOSTNAME', 'docker.cmmcfluxl1um.sa-east-1.rds.amazonaws.com');
-define('DB_USERNAME', 'docker');
-define('DB_PASSWORD', 'DBPASS');
-define('DB_DATABASE', 'OPENCART');
-define('DB_PORT', '3306');
-define('DB_PREFIX', 'oc_');
+  include_once ('/var/www/html/nextdb.php');
 
